5ad897551630658ef27441a4ecf39803098b4199
Carlos Fávero <carlos@baralternativo.com.br>
2021-02-14 15:17:32 -0300

    build client

---
+++ client/build/precache-manifest.49e9d319f64697a216cce6a6fc5790e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a0b65cfeceeeddc6184fc63379859a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a33baef2c19ee34e8f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1e2a993d9fb32471e912",
+    "url": "/static/js/2.11a85806.chunk.js"
+  },
+  {
+    "revision": "f24b473656bdcdf7a40306c1c14414ef",
+    "url": "/static/js/2.11a85806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a33baef2c19ee34e8f",
+    "url": "/static/js/main.0b751c97.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -53,10 +53,10 @@ io.on('connection', socket => {
 });
 
 if(process.env.PROD){
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, './client/build')));
     app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, '/client/build/index.html'));
-    });
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });nop
 };
 
 const port = process.env.PORT || 8000;
